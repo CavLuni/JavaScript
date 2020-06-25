5e31417c29a8a285b1f6438f5599436cd2d2d600
cavluni <denis.luniere@gmail.com>
2020-06-25 16:26:19 -0300

    Promisses

---
+++ node/7promisses.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const { callbackify } = require('util')
+
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise ((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+
+            res.on('data', dados => {
+                resultado += dados
+            })
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+Promise.all([getTurma('A'), getTurma('B'), getTurma('C')/*, getTurma('D')*/]) //caso seja passado um parametro inexistente cairá no bloco catch
+    .then(turmas => [].concat(...turmas))
+    .then(alunos => alunos.map(aluno => aluno.nome))
+    .then(nomes => console.log(nomes))
+    .catch(e => console.log(e.message))
